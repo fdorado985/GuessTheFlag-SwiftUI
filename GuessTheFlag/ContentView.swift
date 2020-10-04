f4d559fdc9a0a500bc6d17e09117f0b575ac64d2
Juan Dorado <fdorado985@gmail.com>
2020-10-04 00:11:16 -0500

    Show an alert and ask new question after continue

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,13 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
-  var countries = [
+  @State var countries = [
     "Estonia", "France", "Germany",
     "Ireland", "Italy", "Nigeria",
     "Poland", "Russia", "Spain",
     "UK", "US"
-  ]
-  var correctAnswer = Int.random(in: 0...2)
+  ].shuffled()
+  @State var correctAnswer = Int.random(in: 0...2)
+  @State private var showingScore = false
+  @State private var scoreTitle = ""
 
   var body: some View {
     ZStack {
@@ -33,7 +35,7 @@ struct ContentView: View {
         ForEach(0 ..< 3) { number in
           Button(
             action: {
-              // flag was tapped
+              self.flagTapped(number)
             },
             label: {
               Image(self.countries[number])
@@ -45,6 +47,36 @@ struct ContentView: View {
         Spacer()
       }
     }
+    .alert(
+      isPresented: $showingScore,
+      content: {
+        Alert(
+          title: Text(scoreTitle),
+          message: Text("Your score is ???"),
+          dismissButton: .default(
+            Text("Continue"),
+            action: {
+              self.askQuestion()
+            }
+          )
+        )
+      }
+    )
+  }
+
+  func flagTapped(_ number: Int) {
+    if number == correctAnswer {
+      scoreTitle = "Correct"
+    } else {
+      scoreTitle = "Wrong"
+    }
+
+    showingScore = true
+  }
+
+  func askQuestion() {
+    countries.shuffle()
+    correctAnswer = Int.random(in: 0...2)
   }
 }
 
